f2a28b296b889a495cd5fc7066c53708db59e376
FireFly3 <4irelly@gmail.com>
2022-07-22 16:46:55 +0200

    JS hw 1

---
+++ Lesson1/HW/task1.js
@@ -0,0 +1,76 @@
+let word1 = 'Hello';
+let word2 = 'owu';
+let word3 = 'com';
+let word4 = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+let boolean1 = true;
+let boolean2 = false;
+
+console.log(word1);
+console.log(word2);
+console.log(word3);
+console.log(word4);
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number6);
+console.log(number7);
+console.log(boolean1);
+console.log(boolean2);
+
+alert(word1);
+alert(word2);
+alert(word3);
+alert(word4);
+alert(number1);
+alert(number2);
+alert(number3);
+alert(number4);
+alert(number5);
+alert(number6);
+alert(number7);
+alert(boolean1);
+alert(boolean2);
+
+document.write(word1);
+document.write(word2);
+document.write(word3);
+document.write(word4);
+document.write(number1);
+document.write(number2);
+document.write(number3);
+document.write(number4);
+document.write(number5);
+document.write(number6);
+document.write(number7);
+document.write(boolean1);
+document.write(boolean2);
+
+let firstName = "Denys ";
+let middleName = "Volodymirovich ";
+let lastName = 'Dyrda';
+
+let person = firstName + middleName + lastName;
+
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let name = prompt('Ім\'я?', 'Введіть текст');
+let name2 = prompt('По-батькові?', 'Введіть текст')
+let age = prompt('Вік?', 'Введіть число');
+console.log(`${name} ${name2}. ${age} роки`);
